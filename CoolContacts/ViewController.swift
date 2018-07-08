521ae55b750b85385dd1262ca00f1ae85da37c7e
DanTheManGude <djg4755@rit.edu>
2018-07-07 20:46:05 -0400

    Select all contacts in one button

--- CoolContacts/ViewController.swift
+++ CoolContacts/ViewController.swift
@@ -11,8 +11,39 @@ import ContactsUI
 
 class ViewController: UIViewController , CNContactPickerDelegate {
 
-    @IBAction func buttonPressed(_ sender: Any) {
-        print("You, clicked the button")
+    @IBAction func allContacts(_ sender: Any) {
+        let store = CNContactStore()
+
+        let keysToFetch = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName)]
+        
+        var allContainers: [CNContainer] = []
+        do {
+            allContainers = try store.containers(matching: nil)
+        } catch {
+            print("Error fetching containers")
+        }
+        
+        var contacts: [CNContact] = []
+        
+        // Loop the containers
+        for container in allContainers {
+            let fetchPredicate = CNContact.predicateForContactsInContainer(withIdentifier: container.identifier)
+            
+            do {
+                let containerResults = try store.unifiedContacts(matching: fetchPredicate, keysToFetch: keysToFetch)
+                // Put them into "contacts"
+                contacts.append(contentsOf: containerResults)
+            } catch {
+                print("Error fetching results for container")
+            }
+        }
+        
+        for contact in contacts {
+            changeContact(contact)
+        }
+    }
+    
+    @IBAction func someContacts(_ sender: UIButton) {
         let cnPicker = CNContactPickerViewController()
         cnPicker.delegate = self
         self.present(cnPicker, animated: true, completion: nil)
@@ -20,28 +51,34 @@ class ViewController: UIViewController , CNContactPickerDelegate {
     
     func contactPicker(_ picker: CNContactPickerViewController, didSelect contacts: [CNContact]) {
         contacts.forEach { contact in
-            let name = contact.givenName + " " + contact.familyName
-            let initials = name.components(separatedBy: " ").reduce("") { ($0 == "" ? "" : "\($0.first!)") + "\($1.first!)" }
             
-            let mutableContact = contact.mutableCopy() as! CNMutableContact
-           
-            let h = Float(arc4random()) / 0xFFFFFFFF
-            let color = "(\(h), 0.5, 0.95)"
             
-            print("Initials: \(initials)")
-            print("Color: \(color)")
-            
-            mutableContact.nickname.append(initials)
-            
-            let store = CNContactStore()
-            let saveRequest = CNSaveRequest()
-            saveRequest.update(mutableContact)
-            try! store.execute(saveRequest)
+            changeContact(contact)
         }
     }
     
     func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
         print("Cancel Contact Picker")
+    }
+    
+    func changeContact(_ contact: CNContact) {
+        let name = contact.givenName + " " + contact.familyName
+        let initials = name.components(separatedBy: " ").reduce("") { ($0 == "" ? "" : "\($0.first!)") + "\($1.first!)" }
+        
+        let mutableContact = contact.mutableCopy() as! CNMutableContact
+        
+        let h = Float(arc4random()) / 0xFFFFFFFF
+        let color = "(\(h), 0.5, 0.95)"
+        
+        print("Initials: \(initials)")
+        print("Color: \(color)")
+        
+        mutableContact.nickname.append(initials)
+        
+        let store = CNContactStore()
+        let saveRequest = CNSaveRequest()
+        saveRequest.update(mutableContact)
+        try! store.execute(saveRequest)
     }
     
     override func viewDidLoad() {
